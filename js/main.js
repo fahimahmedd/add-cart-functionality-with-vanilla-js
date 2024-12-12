d169eacb409d1c320d98819c66f327c968b943f2
fahimahmedd <fahimahmed01568@gmail.com>
2024-12-12 13:10:48 +0600

    before responsive

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // Thumbnail Image Changing with product color buttons
+  // Global Variable
+  const productTitle = document.querySelector(".product-title").textContent;
   const colorButtons = document.querySelectorAll('input[name="product-color"]');
-  const productThumbnail = document.getElementById('productImage');
+  const productThumbnail = document.getElementById("productImage");
+  const qtyInputContainers = document.querySelectorAll(".qty-input-container");
+  const addToCartBtn = document.querySelector(".c-button");
+  const stickyCheckOut = document.querySelector(".checkout-sticky");
+  const productQuantityInput = document.querySelector(".product-qty");
+  const cartModalBody = document.querySelector(".render-table-item");
+  const cartTotalQuantity = document.querySelector(".modal-total-quantity");
+  const cartTotalAmount = document.querySelector(".modal-total-amount");
 
+  let cartCount = 0;
+  let totalAmount = 0;
+  let totalQuantity = 0;
+
+  // Update Thumbnail Image
   colorButtons.forEach((radio) => {
     radio.addEventListener("change", (e) => {
       const newImageSrc = e.target.getAttribute("data-image");
@@ -12,9 +25,7 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 
-  // Add Remove Product Quantity
-  const qtyInputContainers = document.querySelectorAll(".qty-input-container");
-
+  // Handle Quantity Changes
   qtyInputContainers.forEach((container) => {
     const minusBtn = container.querySelector(".qty-count--minus");
     const plusBtn = container.querySelector(".qty-count--add");
@@ -44,23 +55,65 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 
-  // Show sticky checkout button by add to cart button
-  const addToCartBtn = document.querySelector(".c-button");
-  const stickyCheckOut = document.querySelector(".checkout-sticky");
-  const productQuantityInput = document.querySelector(".product-qty");
-  let cartCount = 0;
-
+  // Add Product to Cart
   addToCartBtn.addEventListener("click", () => {
-    const quantity = parseInt(productQuantityInput.value, 10);
+    const quantity = parseInt(productQuantityInput.value);
+    const selectedColorInput = document.querySelector(
+      'input[name="product-color"]:checked'
+    );
+    const selectedColorValue =
+      selectedColorInput?.getAttribute("data-color") || "N/A";
+    const selctedSizeInput = document.querySelector(
+      'input[name="product-size"]:checked'
+    );
+    const selectedSizeValue =
+      selctedSizeInput?.getAttribute("data-size") || "N/A";
+    const selectedSizeAmount =
+      selctedSizeInput?.getAttribute("data-amount") || "N/A";
+    const selectedColorImage =
+      selectedColorInput?.getAttribute("data-image") || "N/A";
 
     if (quantity > 0) {
-      cartCount += quantity;
-      stickyCheckOut.querySelector("span").textContent = cartCount;
+      const product = {
+        title: productTitle,
+        color: selectedColorValue,
+        size: selectedSizeValue,
+        image: selectedColorImage,
+        quantity,
+        totalPrice: selectedSizeAmount * quantity,
+      };
 
+      cartCount += 1;
+      stickyCheckOut.querySelector("span").textContent = cartCount;
       stickyCheckOut.style.opacity = "1";
       stickyCheckOut.style.bottom = "20px";
+
+      totalQuantity += quantity;
+      totalAmount += selectedSizeAmount * quantity;
+
+      updateCart(product);
+
+      cartTotalQuantity.textContent = totalQuantity;
+      cartTotalAmount.textContent = totalAmount.toFixed(2);
     }
   });
 
-  
+  function updateCart(product) {
+    const productRow = `
+      <tr>
+        <td class="d-flex align-items-center">
+          <div class="product-prev-img"><img src="${
+            product.image
+          }" alt="Product image"></div>
+          <span>${product.title}</span>
+        </td>
+        <td class="text-center">${product.color}</td>
+        <td class="text-center"><strong>${product.size}</strong></td>
+        <td class="text-center"><strong>${product.quantity}</strong></td>
+        <td class="text-end"><strong>$${product.totalPrice.toFixed(
+          2
+        )}</strong></td>
+      </tr>`;
+    cartModalBody.insertAdjacentHTML("beforeend", productRow);
+  }
 });
